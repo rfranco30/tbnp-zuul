f5a8669b37211935ed011e900d17c4425ec0675d
Unknown <rodrigofranco30@gmail.com>
2020-05-20 04:00:36 -0300

    initial

---
+++ src/test/java/br/com/bnp/teste/zuul/ZuulGatwayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bnp.teste.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
